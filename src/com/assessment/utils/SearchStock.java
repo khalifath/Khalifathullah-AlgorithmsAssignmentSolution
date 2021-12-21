b4f00f6cf7141747beb32d0116ae6263ab8cfe30
Khalif <Khalifathullah@yahoo.co.uk>
2021-12-21 10:46:54 +0530

    single item fix

--- src/com/assessment/utils/SearchStock.java
+++ src/com/assessment/utils/SearchStock.java
@@ -11,36 +11,37 @@ public class SearchStock {
 		int mid=(left+right)/2;
 		boolean isAvailble=false;
 
-		do
-		{
-			if(left>right || mid>right) {
-				return -1;
-			}
-			
-			if(key==stock[left].getStockPrice()) return left;
-			if(key==stock[mid].getStockPrice()) return mid;
-			if(key==stock[right].getStockPrice()) return right;
-		
-			if ((key>stock[left].getStockPrice() && key<stock[mid].getStockPrice()) || (key<stock[left].getStockPrice() && key>stock[mid].getStockPrice())) {
-				right=mid;
-				mid = (left+right)/2;
-				isAvailble=true;
-			}else if(key<stock[mid].getStockPrice() && key>stock[right].getStockPrice() ||(key<stock[mid].getStockPrice() && key<stock[right].getStockPrice())){
-				left=mid;
-				mid = (left+right)/2;
-				isAvailble=true;
-			}
-			
-			if (!isAvailble) {
-				for (int i=0;i<stock.length;i++) {
-					if(key==stock[i].getStockPrice()) return i;
-				}
-				return -1;
-			}
+
+			do
+			{
 				
+				if(key==stock[left].getStockPrice()) return left;
+				if(key==stock[mid].getStockPrice()) return mid;
+				if(key==stock[right].getStockPrice()) return right;
 
-		}while(left<=right);
+				if(left>=right || mid>=right) {
+					return -1;
+				}
+				
+				if ((key>stock[left].getStockPrice() && key<stock[mid].getStockPrice()) || (key<stock[left].getStockPrice() && key>stock[mid].getStockPrice())) {
+					right=mid;
+					mid = (left+right)/2;
+					isAvailble=true;
+				}else if(key<stock[mid].getStockPrice() && key>stock[right].getStockPrice() ||(key<stock[mid].getStockPrice() && key<stock[right].getStockPrice())){
+					left=mid;
+					mid = (left+right)/2;
+					isAvailble=true;
+				}
 
-		return -1;
-	}
+				if (!isAvailble) {
+					for (int i=0;i<stock.length;i++) {
+						if(key==stock[i].getStockPrice()) return i;
+					}
+					return -1;
+				}
+			
+			}while(left<=right);
+
+			return -1;
+		}
 }
